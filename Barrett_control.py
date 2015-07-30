c0d734b8eb74a47156ecd6e0a8344552f6b30a83
saurabdixit <saurabh.dxt21@gmail.com>
2015-07-30 11:46:49 -0700

    little bit modified

--- Barrett_control.py
+++ Barrett_control.py
@@ -6,24 +6,25 @@ from openravepy.misc import OpenRAVEGlobalArguments
 import sys
 sys.path.append('/stl_dae_files/')
 
-
 def main(env,options):
-     env.Load('Coil1.STL')
-     env.SetViewer('qtcoin')
-     robot = env.GetRobots()[0]
-     env.Load('robots/barrettwam.robot.xml')
-     robot1 = env.GetRobots()[1]
-     with env:
-         T = robot.GetLinks()[1].GetTransform()
-         T2 = robot1.GetLinks()[1].GetTransform()
-         print 'This is transformation matrix : '
-         print T, T2
+	obj = env.ReadTrimeshFile('Coil1.STL');
+	env.SetViewer('qtcoin')
+	env.Load('robots/barrettwam.robot.xml')
+	robot = env.GetRobots()[0]
+	with env:
+	    T = robot.GetLinks()[1].GetTransform()
+	    print 'This is transformation matrix : '
+	    print T
 
-def run(args=None):
-	parser = OptionParser(description='This is interface for controlling the robot in openrave')
-	OpenRAVEGlobalArguments.addOptions(parser)
-	(options,leftargs) = parser.parse_args(args=args)
-	OpenRAVEGlobalArguments.parseAndCreateThreadedUser(options,main,defaultviewer=True)	
+#def run(args=None):
+#	parser = OptionParser(description='This is interface for controlling the robot in openrave')
+#	OpenRAVEGlobalArguments.addOptions(parser)
+#	(options,leftargs) = parser.parse_args(args=args)
+#	OpenRAVEGlobalArguments.parseAndCreateThreadedUser(options,main,defaultviewer=True)	
+#
 
 if __name__=="__main__":
-	run()
+	parser = OptionParser(description='This is interface for controlling the robot in openrave')
+	OpenRAVEGlobalArguments.addOptions(parser)
+	(options,leftargs) = parser.parse_args(args=None)
+	OpenRAVEGlobalArguments.parseAndCreateThreadedUser(options,main,defaultviewer=True)	
