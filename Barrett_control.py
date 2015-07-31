5868266dba5e14ddfc5a6d1ecd23bdacf9baeaeb
saurabdixit <saurabh.dxt21@gmail.com>
2015-07-31 09:50:14 -0700

    This is file to import barrett hand and part in a file

--- Barrett_control.py
+++ Barrett_control.py
@@ -1,30 +1,23 @@
+import threading
+from openravepy import *
+import rospy
 
-import time
-import openravepy
-from optparse import OptionParser
-from openravepy.misc import OpenRAVEGlobalArguments
-import sys
-sys.path.append('/stl_dae_files/')
 
-def main(env,options):
-	obj = env.ReadTrimeshFile('Coil1.STL');
-	env.SetViewer('qtcoin')
+def main():
+	env = Environment()
 	env.Load('robots/barrettwam.robot.xml')
+	env.SetViewer('qtcoin')
 	robot = env.GetRobots()[0]
-	with env:
-	    T = robot.GetLinks()[1].GetTransform()
-	    print 'This is transformation matrix : '
-	    print T
-
-#def run(args=None):
-#	parser = OptionParser(description='This is interface for controlling the robot in openrave')
-#	OpenRAVEGlobalArguments.addOptions(parser)
-#	(options,leftargs) = parser.parse_args(args=args)
-#	OpenRAVEGlobalArguments.parseAndCreateThreadedUser(options,main,defaultviewer=True)	
-#
+	obj = env.ReadKinBodyXMLFile('Coil1.STL')
+	env.Add(obj)
+	try:
+		while True:
+			n=1 
+	except KeyboardInterrupt, e:
+		print e
+	finally:
+		print 'exiting'
+		env.Destroy()
 
 if __name__=="__main__":
-	parser = OptionParser(description='This is interface for controlling the robot in openrave')
-	OpenRAVEGlobalArguments.addOptions(parser)
-	(options,leftargs) = parser.parse_args(args=None)
-	OpenRAVEGlobalArguments.parseAndCreateThreadedUser(options,main,defaultviewer=True)	
+	main()
