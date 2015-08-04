df976625eec75262f38c41fad37458613e2a799e
saurabdixit <saurabh.dxt21@gmail.com>
2015-08-03 18:37:49 -0700

    scaled version of the part

--- Barrett_control.py
+++ Barrett_control.py
@@ -5,16 +5,18 @@ import rospy
 
 def main():
 	env = Environment()
-	env.Load('robots/barrettwam.robot.xml')
+        env.Load('robots/barrettwam.robot.xml')
 	env.SetViewer('qtcoin')
-	robot = env.GetRobots()[0]
-	obj = env.ReadKinBodyXMLFile('Coil1.STL')
-	env.Add(obj)
+        obj = env.ReadKinBodyXMLFile('stl_files/Coil1.STL',{'scalegeometry':'0.001 0.001 0.001'})
+	body = env.GetRobots()[0]
+#	robot = env.GetRobots()[0]
+	#obj = env.ReadKinBodyXMLFile('Coil1.STL')
+        env.Add(obj)
 	try:
 		while True:
 			n=1 
-	except KeyboardInterrupt, e:
-		print e
+	except KeyboardInterrupt, e: 
+                print e
 	finally:
 		print 'exiting'
 		env.Destroy()
