d5aeea662ff31b7890db788291211d61f9ab8626
saurabdixit <saurabh.dxt21@gmail.com>
2015-08-03 19:06:30 -0700

    latest updated

--- Barrett_control.py
+++ Barrett_control.py
@@ -8,10 +8,10 @@ def main():
         env.Load('robots/barrettwam.robot.xml')
 	env.SetViewer('qtcoin')
         obj = env.ReadKinBodyXMLFile('stl_files/Coil1.STL',{'scalegeometry':'0.001 0.001 0.001'})
-	body = env.GetRobots()[0]
-#	robot = env.GetRobots()[0]
-	#obj = env.ReadKinBodyXMLFile('Coil1.STL')
+	robot = env.GetRobots()[0]
         env.Add(obj)
+        T_robot = robot.GetLinkTransformations()
+        print (T_robot)
 	try:
 		while True:
 			n=1 
