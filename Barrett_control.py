df3e2593317fe1dbb68b167afdbe8800896a0015
saurabdixit <saurabh.dxt21@gmail.com>
2015-08-04 17:23:47 -0700

    Working with new robot

--- Barrett_control.py
+++ Barrett_control.py
@@ -1,17 +1,21 @@
 import threading
 from openravepy import *
 import rospy
+import numpy as np
+import sys
 
 
 def main():
 	env = Environment()
-        env.Load('robots/barrettwam.robot.xml')
+        env.Load('barrett_wam.dae')
 	env.SetViewer('qtcoin')
-        obj = env.ReadKinBodyXMLFile('stl_files/Coil1.STL',{'scalegeometry':'0.001 0.001 0.001'})
 	robot = env.GetRobots()[0]
+        obj = env.ReadKinBodyXMLFile('stl_files/Coil1.STL',{'scalegeometry':'0.001 0.001 0.001'})
         env.Add(obj)
-        T_robot = robot.GetLinkTransformations()
-        print (T_robot)
+        T_robot = robot.GetDOFValues()
+        Trans_matrix = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,2.5],[0,0,0,1]])
+        obj.SetTransform(Trans_matrix)
+#        T_robot = robot.SetDOFValues([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 	try:
 		while True:
 			n=1 
