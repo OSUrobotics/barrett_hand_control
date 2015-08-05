48025eb64add357200599ed3c3c55e1f27dcfb68
saurabdixit <saurabh.dxt21@gmail.com>
2015-08-04 19:05:47 -0700

    rosrun working now

--- src/barrett_control.py
+++ src/barrett_control.py
@@ -4,22 +4,21 @@ from openravepy import *
 import rospy
 import numpy as np
 import sys
+import rospkg
+path = rospkg.RosPack().get_path('barrett_hand_control')
 
 
 def main():
 	env = Environment()
-        env.Load('barrett_wam.dae')
+        env.Load(path+'/src/barrett_wam.dae')
 	env.SetViewer('qtcoin')
         robot = env.GetRobots()[0]
-#        print robot
-        obj = env.ReadKinBodyXMLFile('stl_files/Coil1.STL',{'scalegeometry':'0.001 0.001 0.001'})
+        obj = env.ReadKinBodyXMLFile(path+'/src/stl_files/Coil1.STL',{'scalegeometry':'0.001 0.001 0.001'})
         env.Add(obj)
         try:
                 T_robot = robot.GetDOFValues()
-#                print T_robot
                 Trans_matrix = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,2.5],[0,0,0,1]])
                 obj.SetTransform(Trans_matrix)
-        #        T_robot = robot.SetDOFValues([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
                 while True:
                     n=1
 	except KeyboardInterrupt, e: 
@@ -30,4 +29,3 @@ def main():
 
 if __name__=="__main__":
     main()
-    rospy.spin()
