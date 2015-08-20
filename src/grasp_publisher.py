10d9d91117517036f846f4ca1e0ae5d25346de72
saurabdixit <saurabh.dxt21@gmail.com>
2015-08-20 13:41:58 -0700

    grasp_publisher working with hand commands

--- src/grasp_publisher.py
+++ src/grasp_publisher.py
@@ -6,26 +6,20 @@ from std_msgs.msg import Empty
 from math import pi
 import sys,getopt
 import csv
+import threading
 
 class JointPub(object):
-    def __init__(self,vec):
-        rospy.init_node('JointPub',anonymous = True)
+    def __init__(self):
+        rospy.init_node('JointPub',anonymous = True,disable_signals = True)
         self.pub = rospy.Publisher("/bhand/hand_cmd", HandCommand, queue_size=1)
         self.hand_command_template = HandCommand()
-        print "working"
-        self.publish_jnts(vec)
 
     def publish_jnts(self,joint_list):
         self.hand_command_template.f1 = joint_list[10]
         self.hand_command_template.f2 = joint_list[13]
         self.hand_command_template.f3 = joint_list[15]
         self.hand_command_template.spread = joint_list[9]
-        print self.hand_command_template
-        try:
-            while not rospy.is_shutdown():
-                self.pub.publish(self.hand_command_template)
-        except rospy.exceptions.ROSInterruptException:
-            print "closing node"
+        self.pub.publish(self.hand_command_template)
 
 if __name__=="__main__":
     argv = sys.argv[1:]
@@ -57,47 +51,37 @@ if __name__=="__main__":
         vector.append(row_float)
             
     vals = vector[2]
-
+    jnts_obj = JointPub()
     def publish_joint_state():
         global vals
         try:
-            JointPub(vals)
-            valid_entry = 0
-            while not valid_entry:
-                inp = raw_input("Do you want to try another grasp (y/n)")
-                if inp == 'y':
-                    valid_entry = 1
-                    switch_grasp()
-                elif inp == 'n':
-                    valid_entry = 1
-                    print "exiting"
-                    sys.exit(2)
-                else:
-                    print "Enter a valid entry"
-        except:
-            pass
+            while not rospy.is_shutdown():
+                jnts_obj.publish_jnts(vals)
+        except rospy.ROSException:
+            rospy.signal_shutdown("program Killed")
+            print "\n"
 
     def switch_grasp():
         global vals
-        valid_entry = 0
-        while not valid_entry:
-            inp = raw_input( " 1: Highest Range of the grasp \n 2: Lowest Range of grasp \n 3: 1st new grasp \n 4: 2nd new grasp \n Enter your choice : \n")
-            if inp=='1':
-                vals=vector[0]
-                valid_entry = 1
-            elif inp=='2':
-                vals = vector[3]
-                valid_entry = 1
-            elif inp=='3':
-                vals = vector[1]
-                valid_entry =1
-            elif inp =='4':
-                vals = vector[2]
-                valid_entry=1
-            else:
-                print "please enter a valid entry from 1-4: "
+        try:
+            while not rospy.is_shutdown():
+                 inp = raw_input( " \n\n 1: Highest Range of the grasp \n 2: Lowest Range of grasp \n 3: 1st new grasp \n 4: 2nd new grasp \n \n      OR        \n \n<Ctrl-D> to kill program \n \n Enter your choice : ")
+                 if inp=='1':
+                     vals=vector[0]
+                 elif inp=='2':
+                     vals = vector[3]
+                 elif inp=='3':
+                     vals = vector[1]
+                 elif inp =='4':
+                     vals = vector[2]
+                 else:
+                     print "please enter a valid entry from 1-4: "
+        except EOFError:
+            print "\n"
+            rospy.signal_shutdown("program killed")
 
-            if valid_entry==1:
-                publish_joint_state()
-    switch_grasp()
+    t_1 = threading.Thread(name='switch_grasp', target=switch_grasp)
+    t_2 = threading.Thread(name='publish_joint_state', target = publish_joint_state)
+    t_1.start()
+    t_2.start()
 
